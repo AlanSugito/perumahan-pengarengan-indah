3c36676a571c78b9040b03916f09a76ebe4d811f
alansugito <alansugito75@gmail.com>
2023-01-08 09:31:02 +0700

    second commit

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ AOS.init()
 
 const navbar = document.querySelector('.navbar')
 const links = navbar.querySelectorAll('.link')
+const form = document.querySelector('form')
+const pesan = document.querySelector('textarea')
+
 window.addEventListener('scroll', () => {
 
     if(window.scrollY !== 0) {
@@ -11,4 +14,10 @@ window.addEventListener('scroll', () => {
         navbar.style.backgroundColor = 'transparent'
         links.forEach(link => link.style.color = 'white')
     }
+})
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault()
+    const {nama, email, telp} = form
+    swal("Terima Kasih", "Data anda telah tersimpan", 'success')
 })
